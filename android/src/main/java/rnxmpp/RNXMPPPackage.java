17595972d699deddd631253ff584a03a0bc9ef38
Jan <jan.mudzki@gmail.com>
2017-08-17 19:11:46 +0800

    0.47 backward compatibile android support

--- android/src/main/java/rnxmpp/RNXMPPPackage.java
+++ android/src/main/java/rnxmpp/RNXMPPPackage.java
@@ -25,7 +25,7 @@ public class RNXMPPPackage implements ReactPackage{
         return modules;
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
